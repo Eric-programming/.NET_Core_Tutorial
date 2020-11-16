1ab24e05315f7e5e21652593a62be495c17989bf
eric-programming <eric09279@outlook.com>
2020-11-16 14:19:27 -0800

    Init Razor Pages

---
+++ 1_RazorPages/BookApp/Models/Book.cs
@@ -0,0 +1,10 @@
+namespace BookApp.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
